3f3f0ffc9a5ffd96c6b5fa588abec81f6391b837
Naman Dadhich <namankdadhich001@gmail.com>
2022-07-30 01:36:47 +0530

    Day05

--- Amazon/Day05/01. Burning Tree.cpp
+++ Amazon/Day05/01. Burning Tree.cpp
@@ -35,3 +35,62 @@ class Solution {
     }
 };
 // BFS Approach
+class Solution {
+  public:
+    int minTime(Node* root, int target) 
+    {
+        if(!root) 
+            return 0;
+            
+        unordered_map<Node*,Node*> mp;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        Node* t;
+        
+        while(!q.empty()){
+            Node* f=q.front();
+            q.pop();
+            if(f->data==target){
+                t=f;
+            }
+            if(f->left){
+                q.push(f->left);
+                mp[f->left]=f;
+            }
+            if(f->right){
+                q.push(f->right);
+                mp[f->right]=f;
+            }
+        }
+        q.push(t);
+        int timer=0;
+        unordered_map<Node*,bool> vis;
+        while(!q.empty()){
+            int s=q.size();
+            bool burn=false;
+            while(s--){
+                Node* f=q.front();
+                q.pop();
+                if(mp.find(f)!=mp.end() && !vis[mp[f]]){
+                    q.push(mp[f]);
+                    burn=true;
+                }
+                if(f->left && !vis[f->left]){
+                    q.push(f->left);
+                    burn=true;
+                }
+                if(f->right && !vis[f->right]){
+                    q.push(f->right);
+                    burn=true;
+                }
+                vis[f]=true;
+            }
+            if(burn){
+                timer++;
+            }
+        }
+        return timer;
+    }
+};
